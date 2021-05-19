99ae03b82d3ea676c688373e008cce270cb90d19
reslear <reslear@gmail.com>
2021-05-19 18:26:54 +0300

    feat(telegram-bot): add stat for days

--- apps/telegram-bot/README.md
+++ apps/telegram-bot/README.md
@@ -5,10 +5,16 @@
 1. Go to [BotFather](https://t.me/BotFather) and create new bot, and save token
 2. Set environments
 
-   - **Local** - create`.env` file and set your token
+   - **Local**
+
+     1. create`.env` file and set `BOT_TOKEN` your token.
+     2. Generate ssl url `npx localtunnel --port 5000` and set `WEBHOOK_URL`
+
      ```env
      BOT_TOKEN=your_token
+     WEBHOOK_URL=https://smooth-mule-98.loca.lt
      ```
+
    - **Heroku** - go to project `Settings -> Config Vars -> Reveal Config Vars` and insert
 
      | KEY         | VALUE                          |
--- apps/telegram-bot/src/index.ts
+++ apps/telegram-bot/src/index.ts
@@ -1,8 +1,12 @@
 import express, { Request, Response } from 'express'
 import { Telegraf, Markup, Context } from 'telegraf'
-import { checkTalon } from 'edoctor-talon-checker'
 
+import LocalSession from 'telegraf-session-local'
+
+import { checkTalon } from 'edoctor-talon-checker'
 import { CronosTask, scheduleTask } from 'cronosjs'
+import dayjs from 'dayjs'
+
 let PORT = process.env.PORT || 5000
 const WEBHOOK_URL = process.env.WEBHOOK_URL
 const BOT_TOKEN = process.env.BOT_TOKEN
@@ -10,15 +14,32 @@ const BOT_TOKEN = process.env.BOT_TOKEN
 if (!WEBHOOK_URL) throw new Error('"WEBHOOK_URL" env var is required!')
 if (!BOT_TOKEN) throw new Error('"BOT_TOKEN" env var is required!')
 
-const bot = new Telegraf(BOT_TOKEN)
+// Define your own context type
+
+interface Log {
+  [DATE: string]: { time: string; count: number }[]
+}
+interface MyContext extends Context {
+  session: {
+    log: Log
+  }
+}
+
+const bot = new Telegraf<MyContext>(BOT_TOKEN)
 bot.telegram.setWebhook(WEBHOOK_URL + '/secret-path')
+
+bot.use(
+  new LocalSession({
+    database: 'db.json',
+  }).middleware()
+)
 
 const app = express()
 
 const url = 'http://178.124.171.86:8081/4DACTION/TalonyWeb_TalonyList'
 let schedule: CronosTask | null = null
 
-const task = async (ctx: Context, { show_no_talon = false } = {}) => {
+const task = async (ctx: MyContext, { show_no_talon = false } = {}) => {
   const { count } = await checkTalon({
     url,
     form_data: { Check25: 'on' },
@@ -39,6 +60,8 @@ const task = async (ctx: Context, { show_no_talon = false } = {}) => {
       ctx.reply('Талонов нет', { disable_notification: true })
     }
   }
+
+  return { count }
 }
 
 bot.start(async (ctx) => {
@@ -48,14 +71,24 @@ bot.start(async (ctx) => {
       Markup.button.text('/run'),
       Markup.button.text('/stop'),
       Markup.button.text('/check'),
-
-      //
+      Markup.button.text('/stats'),
+      Markup.button.text('/delete'),
     ]).resize()
   )
 })
 
-bot.command('check', async (ctx) => {
-  task(ctx, { show_no_talon: true })
+bot.command('check', async (ctx, next) => {
+  ctx.session.log = ctx.session.log || {}
+
+  const { count } = await task(ctx, { show_no_talon: true })
+
+  const dateKey = dayjs().format('DD.MM.YY')
+  if (!ctx.session.log[dateKey]) ctx.session.log[dateKey] = []
+
+  ctx.session.log[dateKey].push({
+    time: dayjs().format('HH:mm'),
+    count,
+  })
 })
 
 bot.command('run', async (ctx) => {
@@ -79,6 +112,30 @@ bot.command('stop', async (ctx) => {
   } else {
     ctx.replyWithMarkdown('checking not running')
   }
+})
+
+bot.command('stats', (ctx) => {
+  let output = ``
+
+  Object.keys(ctx.session.log).forEach((dateKey) => {
+    output += `*${dateKey}*\n`
+
+    const times = []
+    ctx.session.log[dateKey].map((arr) => {
+      times.push(`${arr.time} - *${arr.count}*`)
+    })
+
+    output += times.join(', ')
+  })
+
+  ctx.replyWithMarkdown(output)
+})
+
+bot.command('/remove', (ctx) => {
+  ctx.replyWithMarkdown(
+    `Removing session from database: \`${JSON.stringify(ctx.session)}\``
+  )
+  ctx.session = null
 })
 
 app.use(bot.webhookCallback('/secret-path'))
